32e645ee8f7055b4487e01de62ad7730bcf989eb
nobletrout <Chuck@MacBook-Pro-6.local>
2016-12-02 21:23:28 -0500

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # vim: sts=2 
 Rails.application.routes.draw do
+  resources :dnsrecords
+
   get 'welcome/index'
   root 'welcome#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

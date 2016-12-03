1e662ab58765efb34a081a99ba942632bcaf9a0f
nobletrout <nobletrout@gmail.com>
2016-12-02 21:38:02 -0500

    foo

--- app/controllers/dnsrecords_controller.rb
+++ app/controllers/dnsrecords_controller.rb
@@ -1,5 +1,20 @@
 # vim: sts=2
 class DnsrecordsController < ApplicationController
   def new
-  end  
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
